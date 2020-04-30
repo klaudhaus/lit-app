9a9f8c83b0fb18cf589dccd112307ef554e85486
tstewart <tstewart@klaudhaus.com>
2020-05-01 09:15:34 +1000

    v 0.0.4 Fix server context

--- contexts/lit-html-server.js
+++ contexts/lit-html-server.js
@@ -1,4 +1,4 @@
-import { html, svg, unsafeHTML, unsafeSVG, render } from "@popeindustries/lit-html-server"
+import { html, svg } from "@popeindustries/lit-html-server"
 
 import { asyncAppend } from "@popeindustries/lit-html-server/directives/async-append"
 import { asyncReplace } from "@popeindustries/lit-html-server/directives/async-replace"
@@ -8,23 +8,19 @@ import { ifDefined } from "@popeindustries/lit-html-server/directives/if-defined
 import { guard } from "@popeindustries/lit-html-server/directives/guard"
 import { repeat } from "@popeindustries/lit-html-server/directives/repeat"
 import { styleMap } from "@popeindustries/lit-html-server/directives/style-map"
+import { unsafeHTML } from "@popeindustries/lit-html-server/directives/unsafe-html"
 import { until } from "@popeindustries/lit-html-server/directives/until"
-
-import { app as litUpApp } from "lit-up"
 
 import { litImp } from "../lit-imp"
 
+export { renderToString, renderToStream, renderToBuffer } from "@popeindustries/lit-html-server"
+
 litImp({
-  html, svg, unsafeHTML, unsafeSVG,
+  html, svg, unsafeHTML,
   asyncAppend, asyncReplace, cache, classMap, ifDefined,
-  guard, repeat, styleMap, templateContent, until
+  guard, repeat, styleMap, until,
+  // Stub substitutes for missing directives
+  live: x => x,
+  templateContent: x => x,
+  unsafeSVG: unsafeHTML,
 })
-
-export const app = ({ model, view, element, boostrap, logger }) => {
-  const wrappedBoostrap = up => {
-    litImp({ up })
-    if (typeof boostrap === "function") return boostrap(up)
-  }
-
-  litUpApp({ model, view, element, bootstrap: wrappedBoostrap, logger, render })
-}
