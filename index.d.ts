7390843766e51e02bf3686be9fb57c62772ce47f
tstewart <tstewart@klaudhaus.com>
2020-09-09 19:53:04 +1000

    v 1.0.0-beta-7 Introduce TS type definitions

---
+++ index.d.ts
@@ -0,0 +1,25 @@
+export {
+  TemplateResult,
+  SVGTemplateResult,
+  html,
+  svg
+} from "lit-html"
+
+export { asyncAppend } from "lit-html/directives/async-append"
+export { asyncReplace } from "lit-html/directives/async-replace"
+export { cache } from "lit-html/directives/cache"
+export { classMap } from "lit-html/directives/class-map"
+export { ifDefined } from "lit-html/directives/if-defined"
+export { guard } from "lit-html/directives/guard"
+export { live } from "lit-html/directives/live"
+export { repeat } from "lit-html/directives/repeat"
+export { styleMap } from "lit-html/directives/style-map"
+export { templateContent } from "lit-html/directives/template-content"
+export { unsafeHTML } from "lit-html/directives/unsafe-html"
+export { unsafeSVG } from "lit-html/directives/unsafe-svg"
+export { until } from "lit-html/directives/until"
+
+type UpOptions = { propagate?: boolean, doDefault?: boolean }
+export function up (handler: (...data: any[]) => any, data?: any, options?: UpOptions) : any
+
+export function fetch (input: RequestInfo, init?: RequestInit) : Promise<Response>
